ba600e90d7a37e03214eb54e0f731099862127e1
AlanF20 <alanif@hotmail.com>
2024-10-31 07:17:08 -0500

    fix; typo

--- deploy.js
+++ deploy.js
@@ -1,12 +1,13 @@
 import hardhat from "hardhat";
 const { ethers } = hardhat;
+
 async function main() {
   try {
     const NFT = await ethers.getContractFactory('Pixel')
     const nfts = await NFT.deploy()
     const txHash = nfts.deployTransaction.hash
     const txReceipt = await ethers.provider.waitForTransaction(txHash)
-    console.log('Contract deployed to address: ', txReceipt.contactAddress)
+    console.log('Contract deployed to address: ', txReceipt.contractAddress)
   } catch (err) {
     console.log(err)
     throw new Error('Error al subir el contrato')
